4dd3b9874ded8a164e8f93503256286c2b42e254
SunilSalve24 <suny.kmr4@gmail.com>
2017-06-30 22:20:08 +0530

    Search widget completed

--- public/js/services/search-widget-service.js
+++ public/js/services/search-widget-service.js
@@ -5,9 +5,12 @@ angular.module('searchTypeheadApp')
     var self = this;
 
     self.dummyDataList =  [
-         {name:'Scott Bryan', email: 'sb@gmail.com', contact: '+91-750-665-4881',  policyNo: '001345221'},
+         {name:'Scott Bryan', email: 'scottb@gmail.com', contact: '+91-750-665-4881',  policyNo: '001345221'},
+         {name:'Aaron Hank', email: 'aronhank@gmail.com', contact: '+91-750-665-4881',  policyNo: '001345221'},
+         {name:'Ann Liebmann', email: 'alieb@gmail.com', contact: '+91-750-665-4881',  policyNo: '001345221'},
          {name:'Scotty Bryan', email: 'sb@gmail.com', contact: '+91-750-665-4881',  policyNo: '001345221'},
-         {name:'Ann Liebmann', email: 'alieb@gmail.com', contact: '+91-750-665-4881',  policyNo: '001345221'}
+         {name:'Chrysostom, John', email: 'johnc@gmail.com', contact: '+91-750-665-4881',  policyNo: '001345221'},
+         {name:'Zappa, Frank', email: 'zapfrank@gmail.com', contact: '+91-750-665-4881',  policyNo: '001345221'}
         ];
           
     self.getData = function(searchText) {
